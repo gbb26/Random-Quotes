07bdcc6dab52114f657c36788107615d0a67c6ae
Gorakshnath <gorakshnath107@gmail.com>
2023-06-21 13:44:01 +0530

    changing ui of the quote-box

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [content, setContent] = useState({
+    text: "",
+    author: "",
+  });
+  const getData = async () => {
+    const url = "https://quotes15.p.rapidapi.com/quotes/random/";
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "267604c5d4mshbb0f82c9db7bf45p157ea4jsnc27e9ce7152e",
+        "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+      },
+    };
 
-  const [content,setContent] = useState({
-    text:'',
-    author:''
-  })
-  const getData = async () =>{
-    const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '267604c5d4mshbb0f82c9db7bf45p157ea4jsnc27e9ce7152e',
-		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-  setContent({
-    text:result.content,
-    author:result.originator.name
-  })
-} catch (error) {
-	console.error(error);
-}
-  }
-  useEffect(()=>{
-    getData()
-  },[])
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setContent({
+        text: result.content,
+        author: result.originator.name,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div id='quote-box' >
-      <div className='content' >
-        <p className='quote' >{content.text}</p>
-        <p className='author' > - {content.author}</p>
+    <div id="quote-box">
+      <div className="content">
+        <p className="quote">{content.text}</p>
+        <p className="author"> - {content.author}</p>
       </div>
-      <center><button
-      onClick={getData}
-      className='btn'>Quote</button></center>
+      <center>
+        <button onClick={getData} className="btn">
+          Quote
+        </button>
+      </center>
     </div>
-  )
+  );
 }
 
 export default App;
